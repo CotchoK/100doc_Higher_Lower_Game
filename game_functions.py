9092b1742b7e3bad5db517add8cc81a6c24932a9
Con <ckritikos@gmail.com>
2024-04-28 21:15:01 +1000

    Complete functioning Higher Lower game

--- game_functions.py
+++ game_functions.py
@@ -1,16 +1,51 @@
 import random as r
+import art as a
 
 # create function to randomly select the person from the list
 def random_selection(list):
     selected = r.choice(list)
     list.pop(list.index(selected))
-    #print(list)
+    #print(new_list)
     return selected
 
 
 # create a function that conducts the comparison
 def compare(person1, person2):
+    """
+    Compares the follower count of the two candidates passed to it
+    :param person1: candidate 1
+    :param person2: candidate 2
+    :return: either 1 or 0
+    """
     if person1["follower_count"] > person2["follower_count"]:
         return 1
     else:
         return 0
+
+
+def game_display(person1, person2, player_lost=False, score=None):
+    """
+    Displays specific text to the player based on what flags are passed to it
+    :param person1: candidate 1 for the game
+    :param person2: candidate 2 for the game
+    :param player_lost: flag if the player lost which prints out alternative messaging
+    :param score: score to print score
+    :return:
+    """
+    if not player_lost:
+        # print the logo
+        print(a.logo)
+
+        # print text for first randomly chosen item from game_data.py item of comparison
+        print(f"Compare A: {person1['name']}, a {person1['description']}, from {person1['country']}")
+
+        # print "vs" art
+        print(a.vs)
+
+        # print text for the secondly randomly chosen item from the game_data.py item of comparison
+        print(f"Compare B: {person2['name']}, a {person2['description']}, from {person2['country']}")
+    else:
+        # print the logo
+        print(a.logo)
+
+        print(f"Sorry, that's wrong. Final score: {score}")
--- main.py
+++ main.py
@@ -1,44 +1,84 @@
 # import required python library
 import random as r
+import os  # used to import the clear screen functionality
 
 # import user defined files
-import art as a
 import game_data as gd
 import game_functions as gf
-
-# print the logo
-print(a.logo)
-
-score = 0
-candidates = gd.data
+import art as a
 
 
-# print text for first randomly chosen item from game_data.py item of comparison
-candidate = gf.random_selection(candidates)
-print(f"Compare A: {candidate['name']}, a {candidate['description']}, from {candidate['country']}")
+def cls():
+    """
+    Clear screen function
+    NOTE: ensure to check the Emulate Terminal in Output Console in the Configurations settings
+    :return:
+    """
+    os.system('cls' if os.name == 'nt' else 'clear')
 
-# print "vs" art
-print(a.vs)
 
-# print text for the secondly randomly chosen item from the game_data.py item of comparison
-candidate2 = gf.random_selection(candidates)
-print(f"Compare B: {candidate2['name']}, a {candidate2['description']}, from {candidate2['country']}")
+def run_game(game_list=gd.data, round_end=False):
+    """
+    Dictates and executes the game loop
+    :param game_list: the starting list of all characters that will be a part of the comparison game
+    :param round_end: flag that indicates whether the round has ended and will determine when to exit game loop
+    :return:
+    """
+    # game variables variables
+    score = 0
+    # create a copy of the game_list to be used, otherwise list mutation will be applied to global variable
+    # the live list is a list that will have reducing number of options the computer can select from.
+    live_list = game_list.copy()
+    # initial candidate selected from the live_list
+    candidate = gf.random_selection(live_list)
 
-# ask user for their input as to whom has the most followers on instagram
-user_choice = input("Who has more followers? Type 'A' or 'B': ").upper()
-if user_choice == 'A':
-    result = gf.compare(candidate, candidate2)
-elif user_choice == 'B':
-    result = gf.compare(candidate2, candidate)
+    # execute round looping
+    while not round_end:
+        # extract the second candidate from the live list
+        candidate2 = gf.random_selection(live_list)
 
-if result == 1:
-    score += 1
-elif result == 0:
-    score
+        # prints the visuals for the player
+        gf.game_display(candidate, candidate2, False)
 
-# we will need to loop the program each time the player gets the answer right
-# we need to increase their score for every right answer
-# if the player gets the answer wrong then the game ends (displaying only heading)
-# and prints the "Sorry, that's wrong. Your final score was: {answers} (no vs logo or comparison texts)
+        # ask user for their input as to whom has the most followers on instagram
+        user_choice = input("Who has more followers? Type 'A' or 'B': ").lower()
 
-# we will need to create another loop to handle when the player wants to play again
+        # execute comparison of both candidates and return a result
+        if user_choice == 'a':
+            result = gf.compare(candidate, candidate2)
+        elif user_choice == 'b':
+            result = gf.compare(candidate2, candidate)
+
+        # with the result execute game logic
+        # increase score and map candidate 1 with candidate 2. will loop again
+        if result == 1:
+            cls()
+            score += 1
+            candidate = candidate2
+        # if player doesn't guess correctly then output a specific sets of visuals on screen
+        # with player score and request to whether they want to play again terminates round loop
+        elif result == 0:
+            cls()
+            gf.game_display(candidate, candidate2, True, score)
+            response = input(f"Did you want to play again 'y' or 'n'? ")
+            round_end = True
+            if response == 'y':
+                run_game()
+            else:
+                exit()
+
+        # checks the live list to see if there are any candidates remaining. if not the player wins the game
+        # asks user if want to play again
+        if len(live_list) == 0:
+            print(a.logo)
+            response = input(f"You have beat the game with {score} points.\n"
+                             "Did you want to play again 'y' or 'n'? ")
+            round_end = True
+            if response == 'y':
+                run_game()
+            else:
+                exit()
+
+
+run_game()
+
